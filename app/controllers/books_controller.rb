e4c0306a9208870f413273ef3c6ff15328f6b869
EC2 Default User <ec2-user@ip-172-31-11-91.ec2.internal>
2020-10-09 09:57:57 +0000

    Homesコントローラ追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,6 +17,22 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(post_book_params)
+    redirect_to book_path(@book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private
   def post_book_params
     params.require(:book).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
 
-
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books, only: [:new, :create, :index, :show]
+  root to: 'homes#top'
+  resources :books
+  resources :users, only: [:show, :index, :edit, :update]
 
 end
